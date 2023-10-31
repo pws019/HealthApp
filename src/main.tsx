f1bf9097efbd81ec7cbe3d212e3ad440fe5a3e20
pws019 <510114321@qq.com>
2023-11-01 01:38:29 +0800

    feat: 完善代码

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import initViewport from './util/viewportAdapter';
+import 'antd-mobile/2x/es/global/global.css'
+
 initViewport();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
